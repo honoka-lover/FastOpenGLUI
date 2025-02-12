7e644ba1c9df85f066ae0b7fd3d5b9d667fab430
honoka-lover <m13068001650@foxmail.com>
2025-02-12 21:39:30 +0800

    test中添加 bit7z 解压rar的示例

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -1,17 +1,45 @@
-//
+﻿//
 // Created by m1393 on 2025/1/17.
 //
-#include <curl/curl.h>
-#include <cstdio>
+#define BIT7Z_AUTO_FORMAT
+
+#include <bit7z/bit7z.hpp>
+#include <bit7z/bitextractor.hpp>
+#include <iostream>
 
 int main() {
-    CURL *curl = curl_easy_init();
-    if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.com");
-        curl_easy_perform(curl);
-        curl_easy_cleanup(curl);
-    } else {
-        fprintf(stderr, "Failed to initialize libcurl\n");
+    try {
+        // 指定 7z.dll 的路径
+        const bit7z::Bit7zLibrary lib{ R"(C:\Program Files\7-Zip\7z.dll)" };
+
+        // **创建 RAR 格式的 BitInFormat 实例**
+        bit7z::BitInFormat rarFormat{ 0x03 };
+        // 创建解压器
+        bit7z::BitExtractor<std::string> extractor{ lib,bit7z::BitFormat::Auto};
+
+        // 要解压的文件
+        std::string rarFile = "D:/test.rar";
+
+        // 目标解压目录
+        std::string outputDir = "D:/test1";
+
+        bit7z::BitArchiveReader reader(lib,rarFile, bit7z::BitFormat::Auto);
+
+
+        uint64_t totalSize = reader.size();
+
+        extractor.setProgressCallback([&](uint64_t progress){
+//                std::cout<<progress<<" "<<totalSize<<std::endl;
+            std::cout<<(float(progress)/totalSize*100)<<std::endl;
+            return true;
+        });
+        // 执行解压
+        extractor.extract(rarFile, outputDir);
+
+        std::cout << "RAR 解压完成！" << std::endl;
+    } catch (const std::exception& e) {
+        std::cerr << "解压失败: " << e.what() << std::endl;
     }
+
     return 0;
 }
